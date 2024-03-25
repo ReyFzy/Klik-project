7a9988f9e53efe0dbcea33d50571518b3b7262aa
ghalib_24 <muhamadghalib7@gmail.com>
2024-03-25 16:41:25 +0700

    feat: change mailer config to smtp config

--- Server-side/src/config/mailerConfig.js
+++
@@ -1,9 +0,0 @@
-export const mailerConfig = {
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false, 
-    auth: {
-        user: 'souris.klik@gmail.com',
-        pass: process.env.EMAIL_PASSWORD
-    }
-};
