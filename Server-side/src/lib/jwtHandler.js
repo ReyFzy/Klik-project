d157874447f2e590750311808af39807eb550c9e
ghalib_24 <muhamadghalib7@gmail.com>
2024-03-25 16:42:13 +0700

    feat: add jwt handler library

---
+++ Server-side/src/lib/jwtHandler.js
@@ -0,0 +1,16 @@
+import jwt from 'jsonwebtoken';
+import { jwtConfig } from '../config/jwtConfig';
+
+export const createToken = (payload, options = {}) => {
+    return jwt.sign(payload, jwtConfig.secretKey, options)
+}
+
+export const verifyToken = (token) => {
+    return new Promise((resolve, rejected) => {
+        jwt.verify(token, jwtConfig.secretKey, (err, decoded) => {
+            if (err) rejected(err);
+
+            resolve(decoded);
+        });
+    });
+}
