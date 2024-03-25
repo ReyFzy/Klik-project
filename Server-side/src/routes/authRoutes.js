a93585d92424aa3602faf335868b732ee6c5f574
ghalib_24 <muhamadghalib7@gmail.com>
2024-03-25 16:42:37 +0700

    feat: add api routes to verify users

--- Server-side/src/routes/authRoutes.js
+++ Server-side/src/routes/authRoutes.js
@@ -1,11 +1,11 @@
 import express from 'express'
-import { authController } from '../controllers/authController.js';
-import { otpVerification } from '../middleware/authMiddleware.js';
 
-const router= express.Router();
+import * as authController from '../controllers/authController.js';
+
+const router = express.Router();
 
 router.post('/auth/register', authController.register);
 router.post('/auth/login', authController.login);
-router.post('/auth/login/otp-verif', otpVerification , authController.login)
+router.post('/auth/verify-user/:email', authController.verifyUser)
 
-export const authRoutes=router;
+export const authRoutes = router;
