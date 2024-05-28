554839019dd19798be8354bd0048407971b8e1ac
ReyFzy <reyzafauzy7@gmail.com>
2024-05-28 10:54:25 +0700

    feat: menambahkan rute untuk logout

--- Server-side/src/routes/authRoutes.js
+++ Server-side/src/routes/authRoutes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router.post('/auth/register', authController.register);
 router.post('/auth/login', authController.login);
-router.post('/auth/verify-user/:email', authController.verifyUser)
+router.post('/auth/verify-user/:email', authController.verifyUser);
+router.post('/auth/logout', authController.logout);
 
 export const authRoutes = router;
