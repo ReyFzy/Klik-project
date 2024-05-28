780883b4423990828f9c88f7e2658e45489da8a4
ReyFzy <reyzafauzy7@gmail.com>
2024-05-28 10:44:33 +0700

    feat: membuat Service untuk upload gambar

---
+++ Server-side/src/services/multerService.js
@@ -0,0 +1,20 @@
+import multer from "multer";
+import path from "path";
+
+export const storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, path.join(process.cwd(), 'uploads'));
+    },
+    filename: function(req, file, cb){
+        cb(null, file.fieldname + Date.now() + '-' + path.extname(file.originalname));
+    }
+});
+
+/**
+ * 
+ * @param {string} picture 
+ */
+
+
+export const uploadAccountPicture = multer({ storage: storage }).single('picture');
+export const uploadProductPicture = multer({ storage: storage }).single('url');
