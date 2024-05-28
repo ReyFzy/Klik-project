6795cd2843411ea4f17b7da29cc9497cfab9ffde
ReyFzy <reyzafauzy7@gmail.com>
2024-05-28 10:55:36 +0700

    feat: membuat rute agar gambar diakses sebagai url

--- Server-side/server.js
+++ Server-side/server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
+import cookieParser from "cookie-parser";
+
 import { userRoutes } from "./src/routes/userRoutes.js";
 import { categoryRoutes } from "./src/routes/categoryRoutes.js";
 import { reviewRoutes } from "./src/routes/reviewRoutes.js";
 import { productRoutes } from "./src/routes/productRoutes.js";
 import { authRoutes } from "./src/routes/authRoutes.js";
-import cookieParser from "cookie-parser";
 import { initLocals } from "./src/middleware/initLocals.js";
 
 const PORT = 5000;
@@ -16,9 +18,13 @@ app.use(cookieParser());
 
 app.use(express.json());
 app.use(initLocals);
+
+app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
+
 app.use('/api/v1', userRoutes);
 app.use('/api/v1', categoryRoutes);
 app.use('/api/v1', productRoutes);
 app.use('/api/v1', reviewRoutes);
 app.use('/api/v1',authRoutes);
+
 app.listen(PORT, ()=> console.log(`server running at port ${PORT}`))
