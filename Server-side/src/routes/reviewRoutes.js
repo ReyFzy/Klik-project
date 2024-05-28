88d57f632b37755720ee4188ced9cea6ee51baa3
ReyFzy <reyzafauzy7@gmail.com>
2024-05-28 10:58:24 +0700

    feat: menambahkan middleware untuk review dan like

--- Server-side/src/routes/reviewRoutes.js
+++ Server-side/src/routes/reviewRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { createReview } from "../controllers/reviewController.js";
+import * as reviewController from "../controllers/reviewController.js";
+import { authenticated } from "../middleware/authenticated.js";
 
 const router = express.Router();
 
-router.post('/review/:product_id', createReview);
+router.post('/review/:product_id', authenticated, reviewController.createReview);
+router.post('/like/:product_id', authenticated, reviewController.createLike);
 
 export const reviewRoutes = router;
